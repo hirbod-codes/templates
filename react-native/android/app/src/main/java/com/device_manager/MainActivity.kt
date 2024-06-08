87c467b4689a9abe48069d8adcacb8605de80d1b
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2024-06-08 13:37:13 +0000

    Update react native

--- react-native/android/app/src/main/java/com/device_manager/MainActivity.kt
+++ react-native/android/app/src/main/java/com/device_manager/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.device_manager
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -7,16 +9,20 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "device_manager"
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    override fun getMainComponentName(): String = "device_manager"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    /**
+    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+    */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null)
+    }
 }
--- react-native/src/App.tsx
+++ react-native/src/App.tsx
@@ -5,15 +5,112 @@
  * @format
  */
 
-import { JSX } from 'react';
-import { Text } from 'react-native';
-import { Title } from 'react-native-paper';
+import { JSX, useState, useCallback, useMemo, useContext } from 'react';
+import { Appearance, TouchableOpacity } from 'react-native';
+import {
+    Appbar,
+    Switch,
+    Title,
+    useTheme,
+    MD3LightTheme as lightTheme,
+    MD3DarkTheme as darkTheme,
+    PaperProvider,
+    Icon,
+    IconButton
+} from 'react-native-paper';
+import { PreferencesContext } from './PreferencesContext'
+import { NavigationContainer, Theme } from '@react-navigation/native';
+import { NativeStackHeaderProps, createNativeStackNavigator } from '@react-navigation/native-stack';
+import { DrawerHeaderProps, createDrawerNavigator } from '@react-navigation/drawer';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App(): JSX.Element {
+    let colorScheme = Appearance.getColorScheme();
+    colorScheme = 'dark'
+
+    const [theme, setTheme] = useState((colorScheme ?? 'light') == 'dark' ? darkTheme : lightTheme)
+
+    // theme = {
+    //     ...theme,
+    //     colors: {
+    //         ...theme.colors,
+    //     },
+    // };
+
+    const toggleTheme = () => {
+        setTheme(theme.dark ? lightTheme : darkTheme)
+    }
+
     return (
         <>
-            <Title>Home</Title>
-            <Text>hello world</Text>
+            <SafeAreaProvider>
+                <PreferencesContext.Provider value={{ toggleTheme }}>
+                    <PaperProvider theme={theme}>
+                        <NavigationContainer theme={theme as unknown as Theme}>
+                            <Drawer.Navigator
+                                initialRouteName="Home"
+                                screenOptions={{
+                                    header: (props) => <CustomAppBar {...props} />,
+                                }}
+                            >
+                                <Drawer.Screen name="Home" component={HomeScreen} />
+                                <Drawer.Screen name="DetailsScreen" component={DetailsScreen} />
+                            </Drawer.Navigator>
+
+                        </NavigationContainer>
+                    </PaperProvider>
+                </PreferencesContext.Provider>
+            </SafeAreaProvider>
+        </>
+    );
+}
+
+function CustomAppBar(props: DrawerHeaderProps) {
+    const theme = useTheme();
+    const { toggleTheme } = useContext(PreferencesContext);
+
+    return (
+        <Appbar.Header mode='small' elevated>
+            <Appbar.Action icon={'menu'} onPress={props.navigation.toggleDrawer} />
+            <Appbar.Content title={props.route.name} />
+            <IconButton icon={'theme-light-dark'} size={20} onPress={toggleTheme} />
+        </Appbar.Header>
+    );
+}
+
+function HomeScreen(props: any): JSX.Element {
+    return (
+        <>
+            <Title>Home Screen</Title>
         </>
     )
+}
+
+function DetailsScreen(props: any): JSX.Element {
+    return (
+        <>
+            <Title>Details Screen</Title>
+        </>
+    )
+}
+
+function Header({ title = null }) {
+    const theme = useTheme();
+    const { toggleTheme } = useContext(PreferencesContext);
+
+    return (
+        <Appbar.Header
+            theme={{
+                colors: {
+                    primary: theme?.colors.surface,
+                },
+            }}
+        >
+            <Appbar.Content title={title} />
+            <Switch value={theme.dark} onValueChange={toggleTheme} />
+        </Appbar.Header>
+    );
 }
